7f455449012ec3d3669bab9f5f90b0c068391fcf
RafaelGustavoPierre <rafaelptjr@gmail.com>
2024-04-22 14:20:03 -0300

    :truck: Rename files .SQL

---
+++ src/main/resources/db/migration/V005__insert_data_table_product.sql
@@ -0,0 +1,5 @@
+INSERT INTO salesAPI.product (name,quantity) VALUES ('Laranja',50);
+INSERT INTO salesAPI.product (name,quantity) VALUES ('Banana',50);
+INSERT INTO salesAPI.product (name,quantity) VALUES ('Cereja',50);
+INSERT INTO salesAPI.product (name,quantity) VALUES ('Uva',50);
+INSERT INTO salesAPI.product (name,quantity) VALUES ('Pinha',50);
