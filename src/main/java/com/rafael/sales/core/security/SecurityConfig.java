2a36314425fb0d6d4df155778fec261791cff23e
RafaelGustavoPierre <rafaelptjr@gmail.com>
2024-10-10 17:03:16 -0300

    :arrow_down: Downgrade dependency Authorization Server OAuth2

--- src/main/java/com/rafael/sales/core/security/SecurityConfig.java
+++ src/main/java/com/rafael/sales/core/security/SecurityConfig.java
@@ -1,10 +1,5 @@
 package com.rafael.sales.core.security;
 
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
-//@Configuration
-//@EnableWebSecurity
 public class SecurityConfig {
 
 }
