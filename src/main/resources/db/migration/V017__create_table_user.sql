9cbbd764986ec75614a4d78f623a0032effac8a8
RafaelGustavoPierre <rafaelptjr@gmail.com>
2024-10-23 18:06:03 -0300

    :rocket: Adding new User migration

---
+++ src/main/resources/db/migration/V017__create_table_user.sql
@@ -0,0 +1,12 @@
+CREATE TABLE user (
+	id BIGINT AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL,
+	name VARCHAR(100) NOT NULL,
+	password VARCHAR(100) NOT NULL,
+	date_register DATETIME NOT NULL,
+	PRIMARY KEY (id)
+);
+
+INSERT INTO `user` (email, name, password, date_register) VALUES ("rafaelrestapi+rafael@gmail.com", "Rafael", "$2a$12$m9907DqXWiPa.rkQRlnh9OGRp0BamkTmORZOo2/PgtXpALCuL6EbG", utc_timestamp);
+INSERT INTO `user` (email, name, password, date_register) VALUES ("rafaelrestapi+telasco@gmail.com", "Telasco", "$2a$12$m9907DqXWiPa.rkQRlnh9OGRp0BamkTmORZOo2/PgtXpALCuL6EbG", utc_timestamp);
+INSERT INTO `user` (email, name, password, date_register) VALUES ("rafaelrestapi+paulinho@gmail.com", "Paulinho", "$2a$12$m9907DqXWiPa.rkQRlnh9OGRp0BamkTmORZOo2/PgtXpALCuL6EbG", utc_timestamp);
