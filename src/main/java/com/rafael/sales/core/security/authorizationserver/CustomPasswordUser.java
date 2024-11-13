2b37e279a4e0ae7a5afee5ccd2e570165c027208
RafaelGustavoPierre <rafaelptjr@gmail.com>
2024-11-13 11:43:45 -0300

    :lock: implemented Password Grant Type

---
+++ src/main/java/com/rafael/sales/core/security/authorizationserver/CustomPasswordUser.java
@@ -0,0 +1,9 @@
+package com.rafael.sales.core.security.authorizationserver;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
+public record CustomPasswordUser(String username, Collection<GrantedAuthority> authorities) {
+
+}
--- src/main/java/com/rafael/sales/core/security/resourceserver/ResourceServerConfig.java
+++ src/main/java/com/rafael/sales/core/security/resourceserver/ResourceServerConfig.java
@@ -14,37 +14,37 @@ import org.springframework.security.oauth2.server.resource.authentication.JwtAut
 import org.springframework.security.oauth2.server.resource.introspection.OpaqueTokenIntrospector;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-@EnableMethodSecurity(prePostEnabled = true)
-@EnableWebSecurity
+//@Configuration
+//@EnableMethodSecurity(prePostEnabled = true)
+//@EnableWebSecurity
 public class ResourceServerConfig {
 
-    @Bean
-    public SecurityFilterChain resourceServerFilterChain(HttpSecurity http, OpaqueTokenIntrospector introspector) throws Exception {
-        http
-                .authorizeHttpRequests((authz) -> authz
-                        .requestMatchers("/oauth2/**", "/login", "/error").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .csrf(AbstractHttpConfigurer::disable)
-                .oauth2ResourceServer(
-                        resourceServer -> resourceServer.jwt(
-                                jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())
-                        )
-                )
-                .headers(headers -> headers
-                        .httpStrictTransportSecurity(HeadersConfigurer.HstsConfig::disable
-                        )
-                );
-        http.formLogin(Customizer.withDefaults());
-        return http.build();
-    }
-
-    @Bean
-    public JwtAuthenticationConverter jwtAuthenticationConverter() {
-        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
-        converter.setJwtGrantedAuthoritiesConverter(new CustomAuthoritiesJwtToken());
-        return converter;
-    }
+//    @Bean
+//    public SecurityFilterChain resourceServerFilterChain(HttpSecurity http, OpaqueTokenIntrospector introspector) throws Exception {
+//        http
+//                .authorizeHttpRequests((authz) -> authz
+//                        .requestMatchers("/oauth2/**", "/login", "/error").permitAll()
+//                        .anyRequest().authenticated()
+//                )
+//                .csrf(AbstractHttpConfigurer::disable)
+//                .oauth2ResourceServer(
+//                        resourceServer -> resourceServer.jwt(
+//                                jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())
+//                        )
+//                )
+//                .headers(headers -> headers
+//                        .httpStrictTransportSecurity(HeadersConfigurer.HstsConfig::disable
+//                        )
+//                );
+//        http.formLogin(Customizer.withDefaults());
+//        return http.build();
+//    }
+//
+//    @Bean
+//    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+//        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+//        converter.setJwtGrantedAuthoritiesConverter(new CustomAuthoritiesJwtToken());
+//        return converter;
+//    }
 
 }
